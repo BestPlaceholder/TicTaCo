87782936f3116140ec3698e4b65e818e5111523c
Hinrik Már Hreinsson <hinrik11@ru.is>
2014-11-02 15:53:44 +0000

    refactured the code so i could cheack is human places two X on same place

--- src/main/java/is/placeholder/tictactoe/TicTacToe.java
+++ src/main/java/is/placeholder/tictactoe/TicTacToe.java
@@ -1,31 +1,37 @@
 package is.placeholder.tictactoe;
 
 public class TicTacToe {
-	public boolean Computer;
-	public boolean Difficulty;
-
+	public boolean computer;
+	public boolean difficulty;
+	public boolean humanTurn;
+	public char grid [][];
 	//An initiatior for tictactoe against human player
-	public TicTacToe(boolean Player){
-		Computer = Player;
+	public TicTacToe(boolean player){
+		computer = player;
+		humanTurn = true;
 	}
 	//An initiatior for tictactoe against computer
-	public TicTacToe(boolean Player, boolean Difficult){
-		Computer = Player;
-		Difficulty = Difficult;
+	public TicTacToe(boolean player, boolean difficult){
+		computer = player;
+		difficulty = difficult;
+	}
+	public char[][] getGrid(){
+		return grid;
 	}
     //Makes a empty tictactoe table
-	public static char[][] makeGrid(){
-		char[][]grid = new char[3][3];
+	public void makeGrid(){
+		grid = new char[3][3];
 		for(int i = 0;i<3;i++){
 			for(int j = 0;j<3;j++){
 				grid[i][j]=' ';
 			}
 		}
-		return grid;
 	}
 
-	public static char[][] humanPlayerMove (int x, int y, char [][] grid){
+	public char[][] humanPlayerMove (int x, int y){
+		
 		grid[x][y] = 'X';
+		humanTurn = false;
 		return grid;
 	}
 
--- src/test/java/is/placeholder/tictactoe/MainTest.java
+++ src/test/java/is/placeholder/tictactoe/MainTest.java
@@ -7,46 +7,60 @@ public class MainTest {
     @Test
     public void TestInitializer() {
     	TicTacToe function = new TicTacToe(true);
-    	assertEquals(true, function.Computer);
+    	assertEquals(true, function.computer);
     }
 	@Test
     public void TestInitializer2() {
     	TicTacToe function = new TicTacToe(false);
-    	assertEquals(false, function.Computer);
+    	assertEquals(false, function.computer);
     }
     @Test
     public void TestInitializerWith2Arguments() {
     	TicTacToe function = new TicTacToe(true, true);
-    	assertEquals(true, function.Computer);
-    	assertEquals(true, function.Difficulty);
+    	assertEquals(true, function.computer);
+    	assertEquals(true, function.difficulty);
     }
     @Test
     public void TestInitializerWith2Arguments2() {
     	TicTacToe function = new TicTacToe(true, false);
-    	assertEquals(true, function.Computer);
-    	assertEquals(false, function.Difficulty);
+    	assertEquals(true, function.computer);
+    	assertEquals(false, function.difficulty);
     }
     @Test
     public void TestmakeGrid() {
     	char [][] test = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
-    	assertArrayEquals( test, TicTacToe.makeGrid());
+    	TicTacToe function = new TicTacToe(true);
+    	function.makeGrid();
+    	assertArrayEquals( test, function.getGrid());
     }
 
     @Test
     public void TestLeftRightCorner() {
+    	TicTacToe function = new TicTacToe(true);
     	char [][] test = {{'X',' ',' '},{' ',' ',' '},{' ',' ',' '}};
-    	char [][] empty = TicTacToe.makeGrid();
-    	assertArrayEquals( test, TicTacToe.humanPlayerMove(0, 0, empty));
+    	function.makeGrid();
+
+    	assertArrayEquals( test, function.humanPlayerMove(0, 0));
     }
 
     @Test
     public void TestManyHumanMovesInRow() {
+    	TicTacToe function = new TicTacToe(true);
     	char [][] test = {{'X',' ','X'},{'X',' ',' '},{'X',' ','X'}};
-    	char [][] empty = TicTacToe.makeGrid();
-    	empty = TicTacToe.humanPlayerMove(0, 0, empty);
-    	empty = TicTacToe.humanPlayerMove(0, 2, empty);
-    	empty = TicTacToe.humanPlayerMove(1, 0, empty);
-    	empty = TicTacToe.humanPlayerMove(2, 0, empty);
-    	assertArrayEquals( test, TicTacToe.humanPlayerMove(2, 2, empty));
+    	function.makeGrid();
+    	function.grid = function.humanPlayerMove(0, 0);
+    	function.grid = function.humanPlayerMove(0, 2);
+    	function.grid = function.humanPlayerMove(1, 0);
+    	function.grid = function.humanPlayerMove(2, 0);
+    	assertArrayEquals( test, function.humanPlayerMove(2, 2));
     }
+    /*
+    @Test
+    public void TestSameHumanMoveInRow() {
+    	TicTacToe function = new TicTacToe(true);
+    	function.test = {{'X',' ',' '},{' ',' ',' '},{' ',' ',' '}};
+    	test = TicTacToe.humanPlayerMove(0, 0, test);
+    	assertArrayEquals( true, function.humanTurn);
+    }
+    */
 }
