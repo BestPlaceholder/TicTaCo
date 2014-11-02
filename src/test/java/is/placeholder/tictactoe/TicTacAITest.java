13e13cc7694b3c00a76ef4f42b7419d5b943cb55
Garviell <faustusxi@gmail.com>
2014-11-02 05:13:00 +0000

    Added a test for first imput for O that fails

--- src/test/java/is/placeholder/tictactoe/TicTacAITest.java
+++ src/test/java/is/placeholder/tictactoe/TicTacAITest.java
@@ -1,23 +1,58 @@
-package is.placeholder.TicTacAITest;
+package is.placeholder.tictactoe;
 import org.junit.*;
 import static org.junit.Assert.*;
-public class TicTacAITest {
-    private TicTacAI AI;
 
+public class TicTacAITest {
     @Test
     public void firstMoveX() {
-        AI = new TicTacAI('X', true);
-        playBoard[][] = {{' ',' ',' '}{' ',' ',' '}{' ',' ',' '}};
-        playBoard[][] = AI.getMove(playBoard);
+        TicTacAI AI = new TicTacAI('X', true);
+        char[][] playBoard = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
+        playBoard = AI.getMove(playBoard);
         assertEquals("First move for X should be 0 0", 'X', playBoard[0][0]);
         playBoard[0][0] = ' ';
         for (int i = 0; i < 3; i++){
             for ( int j = 0; j < 3; j ++){
-                assertEquals("All other fields should be empty", ' ', playBoard[i][j];
-                             }
-		    
-            }
+                assertEquals("All other fields should be empty", ' ', playBoard[i][j]);
+	    }
 	    
-        }
-      
+	}
+	    
     }
+
+    @Test
+    public void firstMoveO() {
+	TicTacAI AI = new TicTacAI('O',true);
+	char[][] playBoard = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
+	for (int i = 0; i < 3 ; i++){
+	    for (int j = 0; j < 3; j++){
+		playBoard[i][j] = 'X';
+		playBoard = AI.getMove(playBoard);
+		if (i == 1 && j == 1){
+		    assertEquals("If the center is occupied circles first move should be 0 0", 'O', playBoard[0][0]);
+		    assertEquals("1 1 should still be X after AI has played", 'X', playBoard[1][1]);
+		    playBoard[0][0] = ' ';
+		    playBoard[1][1] = ' ';
+		    for(int k = 0; k < 3; k++){
+			for (int h = 0; h < 3; h++){
+			    assertEquals("All other fields should be empty", ' ', playBoard[k][h]);
+			}
+		    }
+		}
+		else{
+		    assertEquals("1 1 should be O's first move if possible", 'O', playBoard[1][1]);
+		    assertEquals("X should still be in the same location after AI has played", 'X', playBoard[i][j]);
+		    playBoard[i][j] = ' ';
+                    playBoard[1][1] = ' ';
+		    for(int k = 0; k < 3; k++){
+                        for (int h = 0; h < 3; h++){
+                            assertEquals("All other fields should be empty", ' ', playBoard[k][h]);
+                        }
+                    }
+
+		}
+		    
+	    }
+	}
+    }
+      
+}
