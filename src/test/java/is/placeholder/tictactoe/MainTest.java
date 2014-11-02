57b13460a5a126859a2a7b1345415105fe64d2f7
Hinrik Már Hreinsson <hinrik11@ru.is>
2014-11-02 04:24:17 +0000

    Testing initializer with one argument=false, test fails

--- src/test/java/is/placeholder/tictactoe/MainTest.java
+++ src/test/java/is/placeholder/tictactoe/MainTest.java
@@ -5,10 +5,14 @@ import static org.junit.Assert.*;
 
 public class MainTest {
     @Test
-
     public void TestInitializer() {
     	tictactoe function = new tictactoe(true);
     	assertEquals(true, function.Computer);
+    }
+	@Test
+    public void TestInitializer2() {
+    	tictactoe function = new tictactoe(false);
+    	assertEquals(false, function.Computer);
     }
     
 }
